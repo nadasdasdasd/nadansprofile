3dc4bf4a01373f81a3cd27dbd8b92bcfa9000277
Nada.NisrinaSeptiana <nada.nisrinaseptiana@homecredit.co.id>
2025-11-06 16:39:42 +0700

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,34 @@
 import type React from "react";
 
+type TSection = 'about' | 'work' | 'contact' | 'education'
+const Header: React.FC<any> = () => {
+  const sections: Array<TSection> = ['about', 'work', 'contact', 'education'];
 
- const Header: React.FC<any> = () => {
-  const sections = ['about', 'work', 'contact', 'education'];
-
-  const scrollIntoSection = (section: string) => {
+  const changeUrlPath = (section: TSection) => {
+    window.history.replaceState(null, '', `#${section}`);
+  }
+  const scrollIntoSection = (section: TSection) => {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+      changeUrlPath(section)
     }
   }
   return (
     <>
       <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg">
-       <nav>
-        
-        {sections.map((section) => (
-          <button
-            key={section}
-            onClick={() => scrollIntoSection(section)}
-            style={{ marginRight: 10 }}
+        <nav>
+
+          {sections.map((section) => (
+            <button
+              key={section}
+              onClick={() => scrollIntoSection(section)}
+              style={{ marginRight: 10 }}
             >
               {section.charAt(0).toUpperCase() + section.slice(1)}
             </button>
-            ))}
-       </nav>
+          ))}
+        </nav>
       </header>
     </>
   )
